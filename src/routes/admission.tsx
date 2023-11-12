6c87bd1e8ed947ae0c8fd0af376b4ef6f88fdea9
Norbert <cross@linux.pl>
2023-11-12 19:11:53 +0100

    Add patient info to admission page

--- src/routes/admission.tsx
+++ src/routes/admission.tsx
@@ -1,6 +1,8 @@
-import { Box, Button, Card, Divider, Grid, Sheet, Table, Textarea, Typography } from '@mui/joy';
+// TODO: Move most cards to their own components
+import { Avatar, Badge, Box, Button, Card, Chip, Divider, FormControl, FormLabel, Grid, Input, Sheet, Table, Textarea, Typography } from '@mui/joy';
 import { useParams } from 'react-router-dom';
 import DocumentsTable from '../components/DocumentsTable';
+import { Bloodtype, Favorite, Height, Hotel, HourglassBottom, Scale } from '@mui/icons-material';
 
 interface Admission {
   id: string,
@@ -27,16 +29,60 @@ function AdmissionPage() {
         <Grid xs={12} md={10}>
           <Grid container columns={12} spacing={2}> {/* Need to set columns explicitly for some reason */}
             <Grid xs={12} md={6} lg={6} sm={6} xl={6}>
-              <Card variant='plain' sx={{ width: '100%', minHeight: { xs: 0, md: 300 }, boxShadow: 'sm' }}>
-                Patient Info Here...
+              <Card variant='plain' sx={{ width: '100%', minHeight: { xs: 0, md: 300 }, flexDirection: { xs: 'column', md: 'row' }, boxShadow: 'sm' }}>
+                <Box display='flex' flexDirection='column' alignItems='center' my={3} mx={6}>
+                  <Badge
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                    badgeInset='15%'
+                    color='primary'
+                    badgeContent={<Hotel />} /* HourglassBottom + warning = waiting, Favorite + success = discharged */
+                  >
+                    <Avatar sx={{ width: 80, height: 80 }} />
+                  </Badge>
+                  <Typography level='title-lg' my={2}>John Smith</Typography>
+
+                  <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
+                    <Chip variant='solid' color='primary' size='lg' startDecorator={<Hotel />}>Admitted</Chip>
+                    <Box display='flex' gap={1}>
+                      <Chip variant='soft' color='danger' startDecorator={<Bloodtype />}>B-</Chip>
+                      <Chip variant='soft' startDecorator={<Scale />}>82 kg</Chip>
+                      <Chip variant='soft' startDecorator={<Height />}>180 cm</Chip>
+                    </Box>
+                  </Box>
+                </Box>
+
+                <Divider orientation='vertical' sx={{ display: { xs: 'none', md: 'block' }}} />
+                <Divider orientation='horizontal' sx={{ display: { xs: 'block', md: 'none' }}} />
+
+                <Box display='flex' flexDirection='column' flexGrow={1} gap={2}>
+                  <FormControl>
+                    <FormLabel>Sex</FormLabel>
+                    <Input readOnly variant='soft' value='Male' />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Street</FormLabel>
+                    <Input readOnly variant='soft' value='4242 Some St' />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Address</FormLabel>
+                    <Input readOnly variant='soft' value='Brooklyn, NY, 11209' />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Birthday</FormLabel>
+                    <Input readOnly variant='soft' value='1990-04-20' />
+                  </FormControl>
+                </Box>
               </Card>
             </Grid>
 
             <Grid xs={12} md={6}>
-              <Card variant='plain' sx={{ minHeight: { xs: 0, md: 300 }, boxShadow: 'sm' }}>
+              <Card variant='plain' sx={{ minHeight: { xs: 0, md: 330 }, boxShadow: 'sm' }}>
                 <Typography level='title-md'>History</Typography>
-                <Textarea minRows={7} maxRows={7} defaultValue='Some notes about the patient here...' />
-                <Button>Save</Button>
+                <Textarea minRows={8} maxRows={8} defaultValue='Some notes about the patient here...' />
+                <Button size='lg'>Save</Button>
               </Card>
             </Grid>
 
