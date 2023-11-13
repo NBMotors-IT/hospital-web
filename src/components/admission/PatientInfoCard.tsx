d2cf8fadac74cb42b8465c11813276025d2e5d30
Norbert Budzynski <cross@linux.pl>
2023-11-13 10:16:41 +0100

    Move patient info card in admission to its own component

---
+++ src/components/admission/PatientInfoCard.tsx
@@ -0,0 +1,56 @@
+import { Bloodtype, Height, Hotel, Scale } from '@mui/icons-material';
+import { Avatar, Badge, Box, Chip, Divider, FormControl, FormLabel, Input, Typography } from '@mui/joy';
+
+function PatientInfoCard() {
+  return (
+    <>
+      <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' my={3} mx={2}>
+        <Badge
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          badgeInset='15%'
+          color='primary'
+          badgeContent={<Hotel />} /* HourglassBottom + warning = waiting, Favorite + success = discharged */
+        >
+          <Avatar sx={{ width: 80, height: 80 }} />
+        </Badge>
+        <Typography level='title-lg' my={2}>John Smith</Typography>
+
+        <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
+          <Chip variant='solid' color='primary' size='lg' startDecorator={<Hotel />}>Admitted</Chip>
+          <Box display='flex' gap={1}>
+            <Chip variant='soft' color='danger' startDecorator={<Bloodtype />}>B-</Chip>
+            <Chip variant='soft' startDecorator={<Scale />}>82 kg</Chip>
+            <Chip variant='soft' startDecorator={<Height />}>180 cm</Chip>
+          </Box>
+        </Box>
+      </Box>
+
+      <Divider orientation='vertical' sx={{ display: { xs: 'none', md: 'block' } }} />
+      <Divider orientation='horizontal' sx={{ display: { xs: 'block', md: 'none' } }} />
+
+      <Box display='flex' flexDirection='column' overflow='auto' flexGrow={1} gap={2}>
+        <FormControl>
+          <FormLabel>Sex</FormLabel>
+          <Input readOnly variant='soft' value='Male' />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Street</FormLabel>
+          <Input readOnly variant='soft' value='4242 Some St' />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Address</FormLabel>
+          <Input readOnly variant='soft' value='Brooklyn, NY, 11209' />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Birthday</FormLabel>
+          <Input readOnly variant='soft' value='1990-04-20' />
+        </FormControl>
+      </Box>
+    </>
+  );
+}
+
+export default PatientInfoCard;
--- src/routes/admission.tsx
+++ src/routes/admission.tsx
@@ -1,8 +1,7 @@
-// TODO: Move most cards to their own components
-import { Avatar, Badge, Box, Button, Card, Chip, Divider, FormControl, FormLabel, Grid, Input, Sheet, Table, Textarea, Typography } from '@mui/joy';
+import { Box, Button, Card, Divider, FormControl, FormLabel, Grid, Input, Sheet, Table, Textarea, Typography } from '@mui/joy';
 import { useParams } from 'react-router-dom';
-import DocumentsTable from '../components/DocumentsTable';
-import { Bloodtype, Favorite, Height, Hotel, HourglassBottom, Scale } from '@mui/icons-material';
+import DocumentsTable from '../components/admission/DocumentsTable';
+import PatientInfoCard from '../components/admission/PatientInfoCard';
 
 interface Admission {
   id: string,
@@ -30,51 +29,7 @@ function AdmissionPage() {
           <Grid container columns={12} spacing={2}> {/* Need to set columns explicitly for some reason */}
             <Grid xs={12} md={6}>
               <Card variant='plain' sx={{ width: '100%', height: { md: 330 }, flexDirection: { xs: 'column', md: 'row' }, boxShadow: 'sm' }}>
-                <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' my={3} mx={2}>
-                  <Badge
-                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                    badgeInset='15%'
-                    color='primary'
-                    badgeContent={<Hotel />} /* HourglassBottom + warning = waiting, Favorite + success = discharged */
-                  >
-                    <Avatar sx={{ width: 80, height: 80 }} />
-                  </Badge>
-                  <Typography level='title-lg' my={2}>John Smith</Typography>
-
-                  <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
-                    <Chip variant='solid' color='primary' size='lg' startDecorator={<Hotel />}>Admitted</Chip>
-                    <Box display='flex' gap={1}>
-                      <Chip variant='soft' color='danger' startDecorator={<Bloodtype />}>B-</Chip>
-                      <Chip variant='soft' startDecorator={<Scale />}>82 kg</Chip>
-                      <Chip variant='soft' startDecorator={<Height />}>180 cm</Chip>
-                    </Box>
-                  </Box>
-                </Box>
-
-                <Divider orientation='vertical' sx={{ display: { xs: 'none', md: 'block' }}} />
-                <Divider orientation='horizontal' sx={{ display: { xs: 'block', md: 'none' }}} />
-
-                <Box display='flex' flexDirection='column' overflow='auto' flexGrow={1} gap={2}>
-                  <FormControl>
-                    <FormLabel>Sex</FormLabel>
-                    <Input readOnly variant='soft' value='Male' />
-                  </FormControl>
-
-                  <FormControl>
-                    <FormLabel>Street</FormLabel>
-                    <Input readOnly variant='soft' value='4242 Some St' />
-                  </FormControl>
-
-                  <FormControl>
-                    <FormLabel>Address</FormLabel>
-                    <Input readOnly variant='soft' value='Brooklyn, NY, 11209' />
-                  </FormControl>
-
-                  <FormControl>
-                    <FormLabel>Birthday</FormLabel>
-                    <Input readOnly variant='soft' value='1990-04-20' />
-                  </FormControl>
-                </Box>
+                <PatientInfoCard /> {/* TODO: Pass patient info props */}
               </Card>
             </Grid>
 
