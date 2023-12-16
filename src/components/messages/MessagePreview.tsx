c202732cdaf460b55b5cb2dfd9defa31d516fda5
CrossCRS <cross@linux.pl>
2023-12-16 20:20:05 +0100

    Redesign messages

---
+++ src/components/messages/MessagePreview.tsx
@@ -0,0 +1,50 @@
+import { Avatar, Badge, Box, Card, Typography, useTheme } from "@mui/joy";
+import { Message } from "../../types/message";
+import { Link } from "react-router-dom";
+import { shortTimeFormat, truncateString } from "../../utils";
+import { useMediaQuery } from "@mui/material";
+
+interface Props {
+  message: Message
+}
+
+function MessagePreview({ message } : Props) {
+  const theme = useTheme();
+  const isLg = useMediaQuery(theme.breakpoints.up('lg'));
+  const isMd = useMediaQuery(theme.breakpoints.up('md'));
+  const isSm = useMediaQuery(theme.breakpoints.up('sm'));
+
+  const messageLengthLimit = isMd ? (isLg ? 200 : 100) : (isSm ? 80 : 30);
+
+  return (
+    <Card key={message.id} variant='soft' component={Link} to={`/messages/${message.id}`} sx={{ 
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      p: 1,
+      backgroundColor: 'background.surface',
+      ':hover': {
+        backgroundColor: 'background.level2'
+      },
+      textDecoration: 'none'
+    }}
+  >
+    <Box sx={{ display: 'flex', gap: 3, alignItems: 'center' }}>
+      <Badge invisible={message.read}>
+        <Avatar sx={{ width: 64, height: 64 }} />
+      </Badge>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 0.3 }}>
+        <Typography fontWeight='600'>{message.from}</Typography>
+        <Typography level={message.read ? 'body-sm' : 'title-sm'}>{message.title}</Typography>
+        <Typography level='body-sm'> - {truncateString(message.text, messageLengthLimit)}</Typography>
+      </Box>
+    </Box>
+
+    <Box>
+      <Typography level='body-sm'>{shortTimeFormat(message.sentDate)}</Typography>
+    </Box>
+  </Card>
+  )
+}
+
+export default MessagePreview;
--- src/routes/messages.tsx
+++ src/routes/messages.tsx
@@ -1,22 +1,15 @@
-import { Avatar, Box, Button, Card, IconButton, Table, Typography } from '@mui/joy';
+import { Box, Button, Card, Typography } from '@mui/joy';
 import { Link } from 'react-router-dom';
-import { truncateString } from '../utils';
-import { Create, Delete, Visibility } from '@mui/icons-material';
-
-interface Message {
-  id: string,
-  from: string,
-  title: string,
-  text: string,
-  sent: Date
-}
+import { Create } from '@mui/icons-material';
+import { Message } from '../types/message';
+import MessagePreview from '../components/messages/MessagePreview';
 
 const messages: Message[] = [
-  { id: '4', from: 'Doctor McDoctorface', title: 'Please reply', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sent: new Date('2023-02-13T09:00') },
-  { id: '3', from: 'Doctor McDoctorface', title: 'Hellooooo', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sent: new Date('2023-02-13T08:00') },
-  { id: '2', from: 'Doctor McDoctorface', title: 'Hello again', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sent: new Date('2023-02-13T07:00') },
-  { id: '1', from: 'Doctor McDoctorface', title: 'Hello', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sent: new Date('2023-02-13T06:00') },
-  { id: '0', from: 'Administrator', title: 'Account activation', text: 'Hello, your account has been activated!', sent: new Date('2023-02-12T12:00') }
+  { id: '4', from: 'Doctor McDoctorface', title: 'Please reply', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sentDate: new Date(), read: false },
+  { id: '3', from: 'Doctor McDoctorface', title: 'Hellooooo', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sentDate: new Date('2023-02-12T08:00'), read: false },
+  { id: '2', from: 'Doctor McDoctorface', title: 'Hello again', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sentDate: new Date('2023-02-11T07:00'), read: false },
+  { id: '1', from: 'Doctor McDoctorface', title: 'Hello', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam commodo, diam eget commodo aliquam, dolor tortor interdum metus, eu iaculis ligula nisl non massa. Nulla ut pretium turpis. Morbi ultrices urna sit amet mi volutpat vestibulum. Proin quis enim mauris. Praesent finibus dictum mattis.', sentDate: new Date('2023-02-10T06:00'), read: false },
+  { id: '0', from: 'Administrator', title: 'Account activation', text: 'Hello, your account has been activated!', sentDate: new Date('2023-01-01T12:00'), read: true }
 ]
 
 function MessagesPage() {
@@ -26,39 +19,17 @@ function MessagesPage() {
       <Card>
         <Box><Button component={Link} to={'/messages/new'} variant='soft' startDecorator={<Create />}>New message</Button></Box>
 
-        <Table sx={{
-          '& tr > *:last-child': { textAlign: 'right', width: '20%' },
-          '& tr > *:nth-child(2)': { width: '50%' },
-          '& tr > *': { py: 2 }
-        }}>
-          <tbody>
+        <Box sx={{ 
+            display: 'flex', 
+            flexDirection: 'column', 
+            gap: 1, 
+            width: '100%',
+          }}
+        >
             {messages.map((message) => (
-              <tr key={message.id} style={{ borderRadius: '50%' }}>
-                <td>
-                  <Box sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: { xs: 1, md: 2 },
-                    flexDirection: { xs: 'column', md: 'row' }
-                  }}>
-                    <Avatar sx={{ width: 32, height: 32 }} />
-                    <Typography fontWeight='600' textAlign='center'>{message.from}</Typography>
-                  </Box>
-                </td>
-                <td>
-                  <Typography>
-                    <Typography level='title-sm'>{message.title}</Typography>
-                    <Typography level='body-sm'> - {truncateString(message.text, 50)}</Typography>
-                  </Typography>
-                </td>
-                <td>
-                  <IconButton component={Link} to={`/messages/${message.id}`} variant='plain'><Visibility /></IconButton>
-                  <IconButton component={Link} to={'/'} variant='plain'><Delete /></IconButton>
-                </td>
-              </tr>
+              <MessagePreview message={message} />
             ))}
-          </tbody>
-        </Table>
+        </Box>
       </Card>
     </>
   );
