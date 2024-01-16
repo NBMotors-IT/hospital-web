3cc8d4428e8e77a5e54a2c7c14efdaf0d7c4e04a
Norbert Budzynski <cross@linux.pl>
2024-01-16 13:39:03 +0100

    Refactor PatientInfo to use new PersonInfo component

--- src/components/admission/PatientInfo.tsx
+++ src/components/admission/PatientInfo.tsx
@@ -1,29 +1,26 @@
 import { Bloodtype, Height, Scale } from '@mui/icons-material';
-import { Box, Chip, Divider, FormControl, FormLabel, Input, Typography } from '@mui/joy';
+import { Box, Chip, FormControl, FormLabel, Input } from '@mui/joy';
+import PersonInfo from '../common/PersonInfo';
 import PatientStatusChip from './PatientStatusChip';
 import PatientAvatar from './PatientAvatar';
 
 function PatientInfo() {
   return (
-    <>
-      <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' my={3} mx={2}>
+    <PersonInfo firstName='John' lastName='Smith'>
+      <PersonInfo.Avatar>
         <PatientAvatar status='admitted' />
-        <Typography level='title-lg' my={2}>John Smith</Typography>
+      </PersonInfo.Avatar>
 
-        <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
-          <PatientStatusChip status='admitted' />
-          <Box display='flex' gap={1}>
-            <Chip variant='soft' color='danger' startDecorator={<Bloodtype />}>B-</Chip>
-            <Chip variant='soft' startDecorator={<Scale />}>82 kg</Chip>
-            <Chip variant='soft' startDecorator={<Height />}>180 cm</Chip>
-          </Box>
+      <PersonInfo.Chips>
+        <PatientStatusChip status='admitted' />
+        <Box display='flex' gap={1}>
+          <Chip variant='soft' color='danger' startDecorator={<Bloodtype />}>B-</Chip>
+          <Chip variant='soft' startDecorator={<Scale />}>82 kg</Chip>
+          <Chip variant='soft' startDecorator={<Height />}>180 cm</Chip>
         </Box>
-      </Box>
+      </PersonInfo.Chips>
 
-      <Divider orientation='vertical' inset='context' sx={{ display: { xs: 'none', md: 'block' } }} />
-      <Divider orientation='horizontal' inset='context' sx={{ display: { xs: 'block', md: 'none' } }} />
-
-      <Box display='flex' flexDirection='column' overflow='auto' flexGrow={1} gap={2}>
+      <PersonInfo.Info>
         <FormControl>
           <FormLabel>Sex</FormLabel>
           <Input readOnly variant='soft' value='Male' />
@@ -43,8 +40,8 @@ function PatientInfo() {
           <FormLabel>Birthday</FormLabel>
           <Input readOnly variant='soft' value='1990-04-20' />
         </FormControl>
-      </Box>
-    </>
+      </PersonInfo.Info>
+    </PersonInfo>
   );
 }
 
---
+++ src/components/common/PersonInfo.tsx
@@ -0,0 +1,44 @@
+import { Box, Divider, Typography } from '@mui/joy';
+import { Children } from 'react';
+
+interface Props {
+  firstName: string,
+  lastName: string,
+  children: React.ReactNode
+}
+
+interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+function PersonInfo({ firstName, lastName, children }: Props) {
+  const [avatar, chips, info] = Children.toArray(children);
+
+  return (
+    <>
+      <Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' my={3} mx={2}>
+        {avatar}
+        <Typography level='title-lg' my={2}>{firstName} {lastName}</Typography>
+
+        <Box display='flex' flexDirection='column' alignItems='center' gap={1}>
+          {chips}
+        </Box>
+      </Box>
+
+      <Divider orientation='vertical' inset='context' sx={{ display: { xs: 'none', md: 'block' } }} />
+      <Divider orientation='horizontal' inset='context' sx={{ display: { xs: 'block', md: 'none' } }} />
+
+      <Box display='flex' flexDirection='column' overflow='auto' flexGrow={1} gap={2}>
+        {info}
+      </Box>
+    </>
+  );
+}
+
+const PersonInfoChild = ({ children }: ChildrenProps) => <>{children}</>;
+
+PersonInfo.Avatar = PersonInfoChild;
+PersonInfo.Chips = PersonInfoChild;
+PersonInfo.Info = PersonInfoChild;
+
+export default PersonInfo;
