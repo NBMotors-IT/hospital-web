43c8cb48ece719baf11bd1e5d3ac559208a7bd4b
CrossCRS <cross@linux.pl>
2024-01-23 09:34:10 +0100

    Remove pink panther

--- src/routes/lab/diagnostics.tsx
+++ src/routes/lab/diagnostics.tsx
@@ -12,8 +12,7 @@ function AIDiagnosticsPage() {
       <Breadcrumb links={linksMap} current='AI Diagnostics' />
 
       <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' gap={4}>
-        <Typography level='title-lg' textAlign='center'>TODO</Typography>
-        <img style={{ alignSelf: 'center' }} src='https://i.imgur.com/jxGyuDx.jpeg' />
+        <Typography level='title-lg' textAlign='center'>Work In Progress</Typography>
       </Box>
     </>
   );
