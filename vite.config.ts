d34338b04dbb0e9fd310bc033c97ee72d6338fba
CrossCRS <cross@linux.pl>
2024-02-12 20:32:22 +0100

    Add Progressive Web App support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: false
+      },
+      manifest: {
+        name: 'Hospital',
+        short_name: 'Hospital',
+        theme_color: '#fbfcfe',
+        background_color: '#fbfcfe'
+      }
+    })
+  ],
   server: {
-    port: 3000,
+    port: 3000
   },
   define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    APP_VERSION: JSON.stringify(process.env.npm_package_version)
   }
 });
